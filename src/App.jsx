cdb7e896d869ab9666e5e90ce120fd434ca85a92
Murutu <murutupeter@gmail.com>
2023-04-03 11:10:05 +0300

    chore: implemented yup schema-based form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Register from './pages/Register';
 import RegisterHandleError from './pages/RegisterHandleError';
+import Yup from './pages/Yup';
 
 function App() {
 
   return (
-    <RegisterHandleError />
+    <Yup />
   )
 }
 
